0422d1d0dfd4df47e8a504b249dc8be5095e5941
machageDev <machagefranklyn@gmail.com>
2025-04-27 03:12:41 -1200

    first commit

--- sokosmart/smartsoko/smartsoko/asgi.py
+++ sokosmart/smartsoko/smartsoko/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sokosmart project.
+ASGI config for smartsoko project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sokosmart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartsoko.settings')
 
 application = get_asgi_application()
--- sokosmart/smartsoko/smartsoko/wsgi.py
+++ sokosmart/smartsoko/smartsoko/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sokosmart project.
+WSGI config for smartsoko project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sokosmart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartsoko.settings')
 
 application = get_wsgi_application()
