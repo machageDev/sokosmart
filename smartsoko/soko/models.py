7475641a35a649f8a2693fde3c5e9cc575789d28
machageDev <machagefranklyn@gmail.com>
2025-05-01 23:22:57 -1200

    register / login

--- smartsoko/soko/models.py
+++ smartsoko/soko/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=8,decimal_places=2)
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='product/')
     Category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='product')
     tag = models.CharField(max_length=50,blank=True,null=True)
     is_discounted = models.BooleanField(default=False)
