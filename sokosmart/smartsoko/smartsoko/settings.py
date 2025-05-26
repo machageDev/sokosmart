256a891457ed6caf1a2d3238788142d603aede0f
machageDev <machagefranklyn@gmail.com>
2025-05-26 07:44:12 -1200

    faqs corrected

--- sokosmart/smartsoko/smartsoko/settings.py
+++ sokosmart/smartsoko/smartsoko/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'soko',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -116,8 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'smartsoko', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- sokosmart/smartsoko/soko/models.py
+++ sokosmart/smartsoko/soko/models.py
@@ -11,3 +11,37 @@ class User(models.Model):
     def __str__(self):
         return self.email
     
+class Category(models.Model):
+    name = models.CharField(max_length=245)
+    description = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.name
+   
+    
+class Product(models.Model):
+    CATEGORY_CHOICES =[
+        ('new','New Arrivals'),
+        ('popular','Popular'),
+    ]    
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8,decimal_places=2)
+    image = models.ImageField(upload_to='product/')
+    Category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='product')
+    tag = models.CharField(max_length=50,blank=True,null=True)
+    is_discounted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title    
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    color = models.CharField(max_length=50, blank=True, null=True)
+    size = models.CharField(max_length=10, blank=True, null=True)
+
+    def total_price(self):
+        return self.quantity * self.product.price
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+    
