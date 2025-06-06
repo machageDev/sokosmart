8a43e4a7a8e263457ed5c12c9d25086233f28110
machageDev <machagefranklyn@gmail.com>
2025-06-06 03:14:41 -1200

    removed un neccesry

--- sokosmart/smartsoko/soko/urls.py
+++ sokosmart/smartsoko/soko/urls.py
@@ -24,9 +24,9 @@ urlpatterns = [
     path('api/product/related', views.apirelated_products, name='related-products'),
     path('api/cart/add', views.apiadd_to_cart, name='add-to-cart'),
     path('api/products', views.apiproduct, name='apiproduct-list'),
-    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='forgot_password.html'), name='password_reset'), # type: ignore
-    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
+    path('accounts/password_reset', auth_views.PasswordResetView.as_view(template_name='forgot_password.html'), name='password_reset'), # type: ignore
+    path('accounts/password_reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/reset/done', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
     
 ]
