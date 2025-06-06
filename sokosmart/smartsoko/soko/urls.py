17af1a6d80be082d216796d1e2b148bf2cd63973
machageDev <machagefranklyn@gmail.com>
2025-06-06 03:13:44 -1200

    aloo

--- sokosmart/smartsoko/soko/urls.py
+++ sokosmart/smartsoko/soko/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import PasswordResetView
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -22,7 +24,7 @@ urlpatterns = [
     path('api/product/related', views.apirelated_products, name='related-products'),
     path('api/cart/add', views.apiadd_to_cart, name='add-to-cart'),
     path('api/products', views.apiproduct, name='apiproduct-list'),
-    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='forgot_password.html'), name='password_reset'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='forgot_password.html'), name='password_reset'), # type: ignore
     path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
